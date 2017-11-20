f07d8b7532374a4afcd43bb992ea658cb0a6dd04
Unknown <hosap4ik@gmail.com>
2017-11-20 14:32:19 +0100

    visa saldo

--- apiJavascript.js
+++ apiJavascript.js
@@ -1,3 +1,5 @@
 (function($){
-	
+	$.get('broker.php',params).done(function(response){
+		$('h2.swedbankSaldo').html(response.balances[0].booked.amount.content + response.balances[0].booked.amount.currency);
+	});
 }(jQuery));
