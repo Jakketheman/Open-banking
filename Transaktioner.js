28e2c7ed178ae8a675ba2da7a4cf3678d437c19c
Unknown <hosap4ik@gmail.com>
2017-12-06 01:11:41 +0100

    Allt som jag gjorde måndag tisdag
    Glöm inte göra kopian av eras arbete innan ni pullar det

--- Transaktioner.js
+++ Transaktioner.js
@@ -1,16 +1,28 @@
 (function($){
-	var textbox = $('div.scrollabletextbox');
-	
-	$.get('Transaktioner.php',params).done(function(response){
-		for(var i=1;i>=0;i--){
-			var $tableRow =$('<tr>');
-			var transaktionerTD = '<td class="transaktioner">'+response.transactions.booked[i].amount.content + '</td><td class="valuta">' + response.transactions.booked[i].amount.currency+'</td>';
-			transaktionerTD += '<td class="dataFran">' + response.transactions.booked[i].transaction_date + '</td><td class="dataTill">' + response.transactions.booked[i].value_date+'</td>';
-			$tableRow.append(transaktionerTD);
-			$('table').append($tableRow);
-		}
-	});
-		
-	
-	
+	transaktion();
 }(jQuery));
+function transaktion(){
+	var textbox = $('div.scrollabletextbox');
+	if($('table').find('td.transaktioner').length==0){
+		$.get('Transaktioner.php',params).done(function(response){
+			for(var i=0;i<=1;i++){
+				var $tableRow =$('<tr>');
+				var transaktionerTD = '<td class="transaktioner">'+response.transactions.booked[i].amount.content + '</td><td class="valuta">' + response.transactions.booked[i].amount.currency+'</td>';
+				transaktionerTD += '<td class="dataFran">' + response.transactions.booked[i].transaction_date + '</td><td class="dataTill">' + response.transactions.booked[i].value_date+'</td>';
+				$tableRow.append(transaktionerTD);
+				$('table').append($tableRow);
+			}
+			
+		});
+	}
+		nyTransaktion();
+}
+
+function nyTransaktion(){
+	if(localStorage.getItem('belopp')!==null){
+		var transaktionTD ='<tr><td class="transaktioner"'+localStorage.getItem('saldo')+'</td><td class="valuta">'+'SEK';
+		transaktionTD += '</td><td class="dataFran">idag</td><td class="dataTill">idag</td>';
+		$('table').prepend(transaktionTD);
+		console.log(localStorage.getItem('belopp')+'hello');
+	}
+}
--- apiJavascript.js
+++ apiJavascript.js
@@ -1,26 +1,49 @@
+//Version1.html
 var konto;
+localStorage.removeItem('belopp');
+localStorage.removeItem('saldo');
 (function($){
 	
-	$.get('broker.php',params).done(function(response){
-		 konto={
-		saldo:response.balances[0].booked.amount.content,
-		currency: response.balances[0].booked.amount.currency
-	}
-		$('span.swedbankSaldo').text(konto.saldo + konto.currency);
-		$('button.belopp').on('click',function(){
-			 saldoOverforing();
-		});
-			
-		raknaTotalSaldo(konto.saldo);
+	RaknaSaldo();
+	
+	$('div.sideMenuBar a').on('click',function(e){
+		var href = $(this).attr('href');
+		if(href !=='Transaktioner.html')
+			e.preventDefault();
+		if(href=='Version1.html')
+			$('div.body').load(href +' #main' ,RaknaSaldo);
+		else (href=='Överföring.html')
+			$('div.body').load(href +' #main');
+		history.pushState(null,null,href);
+		console.log('previous page '+history.length);
 	});
 	
-	
+	$(document).on('click','button.belopp',function(){//knapp från Överföring.html 
+		var belopp = $('input#usr').val();
+		console.log(konto.saldo);
+		$.ajax({
+			url:'Version1.html',
+			beforeSend:function(xhr){
+				if(belopp<=konto.saldo){
+					alert('You don\'t have enough money');
+					xhr.abort();
+				}
+			},
+			success:function(){
+				var nySaldo = konto.saldo-belopp;
+				$('span.swedbankSaldo').text(nySaldo);
+				if(window.localStorage){	
+					localStorage.setItem('saldo',nySaldo);
+					localStorage.setItem('belopp',belopp);
+				}$('div.body').load('Version1.html'+ ' #main');
+			}
+		});
+		
+	});
 }(jQuery));
-
 function raknaTotalSaldo(swedbankSaldo){
-	var nordea = $('.nordeaSaldo').text().replace('SEK','');
-	var nordeaSaldo = parseInt(nordea);
-	var totalSaldo = swedbankSaldo + nordeaSaldo;
+	var nordeaSaldo = raknaNordeaSaldo();
+	totalSaldo = nordeaSaldo + konto.saldo;
 	$('span.totalSaldo').html(totalSaldo + konto.currency);
 }
 
@@ -34,15 +57,21 @@ function raknaNordeaSaldo(){
 	var totalSaldo = Nordea1Saldo + Nordea2Saldo;
 
 	$('#NordeaSumma').text(totalSaldo + "SEK");
+	return totalSaldo;
 }
-function saldoOverforing(){
-	var saldo = konto.saldo;
-	var swedbankSaldoText = $('span.swedbankSaldo').text();
-	var swedbankSaldoPengar = parseInt(swedbankSaldoText);
-	var inputText = $('input#usr').text();
-	var textBelopp = parseInt(inputText);
-	swedbankSaldoText = swedbankSaldoPengar - textBelopp;
-	localStorage.setItem('swedbank',swedbankSaldoText);
-	$('span.swedbankSaldo').text(swedbankSaldoText + konto.currency);
-	
+function RaknaSaldo(){
+	$.get('broker.php',params).done(function(response){
+		 konto={
+			saldo:response.balances[0].booked.amount.content,
+			currency: response.balances[0].booked.amount.currency
+		}
+		if(localStorage.getItem('saldo') == undefined)
+			$('span.swedbankSaldo').text(konto.saldo + konto.currency);
+		else{
+			konto.saldo == localStorage.getItem('saldo');
+			$('span.swedbankSaldo').text(konto.saldo + konto.currency);
+		}
+		raknaTotalSaldo(konto.saldo);
+	});
 }
+
